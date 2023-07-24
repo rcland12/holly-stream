4a10f3af79b4dd06b335718b76d5265ef0d082a6
Russell Land <rl02898@georgiasouthern.edu>
2023-07-24 19:15:58 -0400

    indexing thingy

--- models/yolo.py
+++ models/yolo.py
@@ -80,7 +80,7 @@ class Detect(nn.Module):
         t = self.anchors[i].dtype
         shape = 1, self.na, ny, nx, 2
         y, x = torch.arange(ny, device=d, dtype=t), torch.arange(nx, device=d, dtype=t)
-        yv, xv = torch.meshgrid(y, x, indexing='ij')
+        yv, xv = torch.meshgrid(y, x)
         grid = torch.stack((xv, yv), 2).expand(shape) - 0.5
         anchor_grid = (self.anchors[i] * self.stride[i]).view((1, self.na, 1, 1, 2)).expand(shape)
         return grid, anchor_grid
