abe857ae61e6ac55f113f3291f0ec71032cac0ef
Russell Land <rl02898@georgiasouthern.edu>
2023-07-24 15:00:07 -0400

    add yolo code

---
+++ yolo.py
@@ -0,0 +1,218 @@
+import math
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from models.common import (Conv, DWConv)
+
+
+def scale_img(img, ratio=1.0, same_shape=False, gs=32):
+    if ratio == 1.0:
+        return img
+    h, w = img.shape[2:]
+    s = (int(h * ratio), int(w * ratio))
+    img = F.interpolate(img, size=s, mode='bilinear', align_corners=False)
+    if not same_shape:
+        h, w = (math.ceil(x * ratio / gs) * gs for x in (h, w))
+    return F.pad(img, [0, w - s[1], 0, h - s[0]], value=0.447)
+    
+def fuse_conv_and_bn(conv, bn):
+    fusedconv = nn.Conv2d(conv.in_channels,
+                          conv.out_channels,
+                          kernel_size=conv.kernel_size,
+                          stride=conv.stride,
+                          padding=conv.padding,
+                          groups=conv.groups,
+                          bias=True).requires_grad_(False).to(conv.weight.device)
+
+    w_conv = conv.weight.clone().view(conv.out_channels, -1)
+    w_bn = torch.diag(bn.weight.div(torch.sqrt(bn.eps + bn.running_var)))
+    fusedconv.weight.copy_(torch.mm(w_bn, w_conv).view(fusedconv.weight.shape))
+
+    b_conv = torch.zeros(conv.weight.size(0), device=conv.weight.device) if conv.bias is None else conv.bias
+    b_bn = bn.bias - bn.weight.mul(bn.running_mean).div(torch.sqrt(bn.running_var + bn.eps))
+    fusedconv.bias.copy_(torch.mm(w_bn, b_conv.reshape(-1, 1)).reshape(-1) + b_bn)
+    return fusedconv
+
+class Detect(nn.Module):
+    stride = None
+    onnx_dynamic = False
+    export = False
+
+    def __init__(self, nc=80, anchors=(), ch=(), inplace=True):
+        super().__init__()
+        self.nc = nc
+        self.no = nc + 5
+        self.nl = len(anchors)
+        self.na = len(anchors[0]) // 2
+        self.grid = [torch.zeros(1)] * self.nl
+        self.anchor_grid = [torch.zeros(1)] * self.nl
+        self.register_buffer('anchors', torch.tensor(anchors).float().view(self.nl, -1, 2))
+        self.m = nn.ModuleList(nn.Conv2d(x, self.no * self.na, 1) for x in ch)
+        self.inplace = inplace
+
+    def forward(self, x):
+        z = []
+        for i in range(self.nl):
+            x[i] = self.m[i](x[i])
+            bs, _, ny, nx = x[i].shape
+            x[i] = x[i].view(bs, self.na, self.no, ny, nx).permute(0, 1, 3, 4, 2).contiguous()
+
+            if not self.training:
+                if self.onnx_dynamic or self.grid[i].shape[2:4] != x[i].shape[2:4]:
+                    self.grid[i], self.anchor_grid[i] = self._make_grid(nx, ny, i)
+
+                y = x[i].sigmoid()
+                if self.inplace:
+                    y[..., 0:2] = (y[..., 0:2] * 2 + self.grid[i]) * self.stride[i]
+                    y[..., 2:4] = (y[..., 2:4] * 2) ** 2 * self.anchor_grid[i]
+                else:
+                    xy, wh, conf = y.split((2, 2, self.nc + 1), 4)
+                    xy = (xy * 2 + self.grid[i]) * self.stride[i]
+                    wh = (wh * 2) ** 2 * self.anchor_grid[i]
+                    y = torch.cat((xy, wh, conf), 4)
+                z.append(y.view(bs, -1, self.no))
+
+        return x if self.training else (torch.cat(z, 1),) if self.export else (torch.cat(z, 1), x)
+
+    def _make_grid(self, nx=20, ny=20, i=0):
+        d = self.anchors[i].device
+        t = self.anchors[i].dtype
+        shape = 1, self.na, ny, nx, 2
+        y, x = torch.arange(ny, device=d, dtype=t), torch.arange(nx, device=d, dtype=t)
+        yv, xv = torch.meshgrid(y, x, indexing='ij')
+        grid = torch.stack((xv, yv), 2).expand(shape) - 0.5
+        anchor_grid = (self.anchors[i] * self.stride[i]).view((1, self.na, 1, 1, 2)).expand(shape)
+        return grid, anchor_grid
+
+class Model(nn.Module):
+    def __init__(self, cfg='yolov5s.yaml', ch=3, nc=None, anchors=None):
+        super().__init__()
+        if isinstance(cfg, dict):
+            self.yaml = cfg
+        else:
+            import yaml
+            self.yaml_file = Path(cfg).name
+            with open(cfg, encoding='ascii', errors='ignore') as f:
+                self.yaml = yaml.safe_load(f)
+
+        ch = self.yaml['ch'] = self.yaml.get('ch', ch)
+        if nc and nc != self.yaml['nc']:
+            self.yaml['nc'] = nc
+        if anchors:
+            self.yaml['anchors'] = round(anchors)
+        self.model, self.save = parse_model(deepcopy(self.yaml), ch=[ch])
+        self.names = [str(i) for i in range(self.yaml['nc'])]
+        self.inplace = self.yaml.get('inplace', True)
+
+        m = self.model[-1]
+        if isinstance(m, Detect):
+            s = 256
+            m.inplace = self.inplace
+            m.stride = torch.tensor([s / x.shape[-2] for x in self.forward(torch.zeros(1, ch, s, s))])
+            check_anchor_order(m)
+            m.anchors /= m.stride.view(-1, 1, 1)
+            self.stride = m.stride
+            self._initialize_biases()
+
+        initialize_weights(self)
+        self.info()
+
+    def forward(self, x, augment=False, profile=False, visualize=False):
+        if augment:
+            return self._forward_augment(x)
+        return self._forward_once(x, profile, visualize)
+
+    def _forward_augment(self, x):
+        img_size = x.shape[-2:]
+        s = [1, 0.83, 0.67]
+        f = [None, 3, None]
+        y = []
+        for si, fi in zip(s, f):
+            xi = scale_img(x.flip(fi) if fi else x, si, gs=int(self.stride.max()))
+            yi = self._forward_once(xi)[0]
+            yi = self._descale_pred(yi, fi, si, img_size)
+            y.append(yi)
+        y = self._clip_augmented(y)
+        return torch.cat(y, 1), None
+
+    def _forward_once(self, x, profile=False, visualize=False):
+        y, dt = [], []
+        for m in self.model:
+            if m.f != -1:
+                x = y[m.f] if isinstance(m.f, int) else [x if j == -1 else y[j] for j in m.f]
+            if profile:
+                self._profile_one_layer(m, x, dt)
+            x = m(x)
+            y.append(x if m.i in self.save else None)
+            if visualize:
+                feature_visualization(x, m.type, m.i, save_dir=visualize)
+        return x
+
+    def _descale_pred(self, p, flips, scale, img_size):
+        if self.inplace:
+            p[..., :4] /= scale
+            if flips == 2:
+                p[..., 1] = img_size[0] - p[..., 1]
+            elif flips == 3:
+                p[..., 0] = img_size[1] - p[..., 0]
+        else:
+            x, y, wh = p[..., 0:1] / scale, p[..., 1:2] / scale, p[..., 2:4] / scale
+            if flips == 2:
+                y = img_size[0] - y
+            elif flips == 3:
+                x = img_size[1] - x
+            p = torch.cat((x, y, wh, p[..., 4:]), -1)
+        return p
+
+    def _clip_augmented(self, y):
+        nl = self.model[-1].nl
+        g = sum(4 ** x for x in range(nl))
+        e = 1
+        i = (y[0].shape[1] // g) * sum(4 ** x for x in range(e))
+        y[0] = y[0][:, :-i]
+        i = (y[-1].shape[1] // g) * sum(4 ** (nl - 1 - x) for x in range(e))
+        y[-1] = y[-1][:, i:]
+        return y
+
+    def _profile_one_layer(self, m, x, dt):
+        c = isinstance(m, Detect)
+        o = thop.profile(m, inputs=(x.copy() if c else x,), verbose=False)[0] / 1E9 * 2 if thop else 0  # FLOPs
+        t = time_sync()
+        for _ in range(10):
+            m(x.copy() if c else x)
+        dt.append((time_sync() - t) * 100)
+
+    def _initialize_biases(self, cf=None):
+        m = self.model[-1]
+        for mi, s in zip(m.m, m.stride):
+            b = mi.bias.view(m.na, -1).detach()
+            b[:, 4] += math.log(8 / (640 / s) ** 2)
+            b[:, 5:] += math.log(0.6 / (m.nc - 0.999999)) if cf is None else torch.log(cf / cf.sum())
+            mi.bias = torch.nn.Parameter(b.view(-1), requires_grad=True)
+
+    def _print_biases(self):
+        m = self.model[-1]
+        for mi in m.m:
+            b = mi.bias.detach().view(m.na, -1).T
+    
+    def fuse(self):
+        for m in self.model.modules():
+            if isinstance(m, (Conv, DWConv)) and hasattr(m, 'bn'):
+                m.conv = fuse_conv_and_bn(m.conv, m.bn)
+                delattr(m, 'bn')
+                m.forward = m.forward_fuse
+        return self
+
+    def info(self, verbose=False, img_size=640):
+        model_info(self, verbose, img_size)
+
+    def _apply(self, fn):
+        self = super()._apply(fn)
+        m = self.model[-1]
+        if isinstance(m, Detect):
+            m.stride = fn(m.stride)
+            m.grid = list(map(fn, m.grid))
+            if isinstance(m.anchor_grid, list):
+                m.anchor_grid = list(map(fn, m.anchor_grid))
+        return self
