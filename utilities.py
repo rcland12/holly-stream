78b80930bd3617915c94a43fd6ea64647d57bdef
Russell Land <russ@AIML-WP-DFJBC93.SERN.MIL>
2023-07-24 16:47:31 -0400

    add api utils

---
+++ utilities.py
@@ -0,0 +1,172 @@
+import cv2 as cv
+import numpy as np
+
+import torch
+import torchvision
+
+from models.common import Ensemble
+
+
+def normalize_boxes(bbox, width, height):
+    return [float(bbox[0])/width, float(bbox[1])/height, float(bbox[2])/width, float(bbox[3])/height]
+
+def letterbox(im, new_shape=(640, 640), color=(114, 114, 114), auto=True, scaleFill=False, scaleup=True, stride=32):
+    shape = im.shape[:2]
+    if isinstance(new_shape, int):
+        new_shape = (new_shape, new_shape)
+    
+    r = min(new_shape[0] / shape[0], new_shape[1] / shape[1])
+    if not scaleup:
+        r = min(r, 1.0)
+    
+    ratio = r, r
+    new_unpad = int(round(shape[1] * r)), int(round(shape[0] * r))
+    dw, dh = new_shape[1] - new_unpad[0], new_shape[0] - new_unpad[1]
+    if auto:
+        dw, dh = np.mod(dw, stride), np.mod(dh, stride)
+    elif scaleFill:
+        dw, dh = 0.0, 0.0
+        new_unpad = (new_shape[1], new_shape[0])
+        ratio = new_shape[1] / shape[1], new_shape[0] / shape[0]
+    
+    dw /= 2
+    dh /= 2
+    
+    if shape[::-1] != new_unpad:
+        im = cv.resize(im, new_unpad, interpolation=cv.INTER_LINEAR)
+    top, bottom = int(round(dh - 0.1)), int(round(dh + 0.1))
+    left, right = int(round(dw - 0.1)), int(round(dw + 0.1))
+    im = cv.copyMakeBorder(im, top, bottom, left, right, cv.BORDER_CONSTANT, value=color)
+    return im, ratio, (dw, dh)
+
+def attempt_load(weights, device=None, inplace=True, fuse=True):
+    model = Ensemble()
+    for w in weights if isinstance(weights, list) else [weights]:
+        ckpt = torch.load(w, map_location="cpu")
+        ckpt = (ckpt.get("ema") or ckpt["model"]).to(device).float()
+        model.append(ckpt.fuse().eval() if fuse else ckpt.eval())
+    
+    if len(model) == 1:
+        return model[-1]
+    
+    for k in "names", "nc", "yaml":
+        setattr(model, k, getattr(model[0], k))
+    
+    model.stride = model[torch.argmax(torch.tensor([m.stride.max() for m in model])).int()].stride
+    assert all(model[0].nc == m.nc for m in model), f"Models have different class counts: {[m.nc for m in model]}"
+    return model
+
+def scale_coords(img1_shape, coords, img0_shape, ratio_pad=None):
+    if ratio_pad is None:
+        gain = min(img1_shape[0] / img0_shape[0], img1_shape[1] / img0_shape[1])
+        pad = (img1_shape[1] - img0_shape[1] * gain) / 2, (img1_shape[0] - img0_shape[0] * gain) / 2
+    else:
+        gain = ratio_pad[0][0]
+        pad = ratio_pad[1]
+    
+    coords[:, [0, 2]] -= pad[0]
+    coords[:, [1, 3]] -= pad[1]
+    coords[:, :4] /= gain
+    
+    if isinstance(coords, torch.Tensor):
+        coords[:, 0].clamp_(0, img0_shape[1])
+        coords[:, 1].clamp_(0, img0_shape[0])
+        coords[:, 2].clamp_(0, img0_shape[1])
+        coords[:, 3].clamp_(0, img0_shape[0])
+    else:
+        coords[:, [0, 2]] = coords[:, [0, 2]].clip(0, img0_shape[1])
+        coords[:, [1, 3]] = coords[:, [1, 3]].clip(0, img0_shape[0])
+    return coords
+
+def xywh2xyxy(x):
+    y = x.clone() if isinstance(x, torch.Tensor) else np.copy(x)
+    y[:, 0] = x[:, 0] - x[:, 2] / 2
+    y[:, 1] = x[:, 1] - x[:, 3] / 2
+    y[:, 2] = x[:, 0] + x[:, 2] / 2
+    y[:, 3] = x[:, 1] + x[:, 3] / 2
+    return y
+
+def box_area(box):
+    return (box[2] - box[0]) * (box[3] - box[1])
+
+def box_iou(box1, box2, eps=1e-7):
+    (a1, a2), (b1, b2) = box1[:, None].chunk(2, 2), box2.chunk(2, 1)
+    inter = (torch.min(a2, b2) - torch.max(a1, b1)).clamp(0).prod(2)
+    return inter / (box_area(box1.T)[:, None] + box_area(box2.T) - inter + eps)
+
+def non_max_suppression(prediction, conf_thres=0.25, iou_thres=0.45, classes=None, agnostic=False, multi_label=False, labels=(), max_det=300):
+    bs = prediction.shape[0]
+    nc = prediction.shape[2] - 5
+    xc = prediction[..., 4] > conf_thres
+
+    # Checks
+    assert 0 <= conf_thres <= 1, f'Invalid Confidence threshold {conf_thres}, valid values are between 0.0 and 1.0'
+    assert 0 <= iou_thres <= 1, f'Invalid IoU {iou_thres}, valid values are between 0.0 and 1.0'
+
+    # Settings
+    max_wh = 7680
+    max_nms = 30000
+    time_limit = 0.3 + 0.03 * bs
+    redundant = True
+    multi_label &= nc > 1
+    merge = True
+
+    output = [torch.zeros((0, 6), device=prediction.device)] * bs
+    for xi, x in enumerate(prediction):
+        # Apply constraints
+        x = x[xc[xi]]
+
+        # Cat apriori labels if autolabelling
+        if labels and len(labels[xi]):
+            lb = labels[xi]
+            v = torch.zeros((len(lb), nc + 5), device=x.device)
+            v[:, :4] = lb[:, 1:5]
+            v[:, 4] = 1.0
+            v[range(len(lb)), lb[:, 0].long() + 5] = 1.0  # cls
+            x = torch.cat((x, v), 0)
+
+        # If none remain process next image
+        if not x.shape[0]:
+            continue
+
+        # Compute conf
+        x[:, 5:] *= x[:, 4:5]
+
+        # Box (center x, center y, width, height) to (x1, y1, x2, y2)
+        box = xywh2xyxy(x[:, :4])
+
+        # Detections matrix nx6 (xyxy, conf, cls)
+        if multi_label:
+            i, j = (x[:, 5:] > conf_thres).nonzero(as_tuple=False).T
+            x = torch.cat((box[i], x[i, j + 5, None], j[:, None].float()), 1)
+        else:
+            conf, j = x[:, 5:].max(1, keepdim=True)
+            x = torch.cat((box, conf, j.float()), 1)[conf.view(-1) > conf_thres]
+
+        # Filter by class
+        if classes is not None:
+            x = x[(x[:, 5:6] == torch.tensor(classes, device=x.device)).any(1)]
+
+        # Check shape
+        n = x.shape[0]
+        if not n:
+            continue
+        elif n > max_nms:
+            x = x[x[:, 4].argsort(descending=True)[:max_nms]]
+
+        # Batched NMS
+        c = x[:, 5:6] * (0 if agnostic else max_wh)
+        boxes, scores = x[:, :4] + c, x[:, 4]
+        i = torchvision.ops.nms(boxes, scores, iou_thres)
+        if i.shape[0] > max_det:
+            i = i[:max_det]
+        if merge and (1 < n < 3E3):
+            iou = box_iou(boxes[i], boxes) > iou_thres
+            weights = iou * scores[None]
+            x[i, :4] = torch.mm(weights, x[:, :4]).float() / weights.sum(1, keepdim=True)
+            if redundant:
+                i = i[iou.sum(1) > 1]
+
+        output[xi] = x[i]
+
+    return output
