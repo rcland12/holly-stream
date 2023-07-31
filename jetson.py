474bea6bf366c4036d27c4d5ba27174067a179e1
Russell Land <rl02898@georgiasouthern.edu>
2023-07-30 20:25:44 -0400

    add ffmpeg capabilities to jetson file

--- jetson.py
+++ jetson.py
@@ -1,4 +1,5 @@
 import cv2
+import subprocess
 
 from assets import Assets
 from nanocamera import Camera
@@ -6,49 +7,86 @@ from model import ObjectDetection
 from utilities import EnvArgumentParser
 
 
-def main(model_path, classes, width, height, fps):
-    assets = Assets()
-    model = ObjectDetection(model_path)
+def main(
+		model_path,
+		classes,
+		stream_ip,
+		stream_port,
+		stream_application,
+		stream_key,
+		camera_index,
+		camera_width,
+		camera_height,
+		camera_fps
+	):
+	
+	assets = Assets()
+	model = ObjectDetection(model_path)
 
-    camera = Camera(flip=0, width=width, height=height, fps=fps)
-    while camera.isReady():
-        try:
-            frame = camera.read()
-            objs = model(frame=frame, classes=classes)
+	rtmp_url = "rtmp://{}:{}/{}/{}".format(
+		stream_ip,
+		stream_port,
+		stream_application,
+		stream_key
+	)
 
-            for obj in objs:
-                score = obj['score']
-                label = obj['label']
-                xmin, ymin, xmax, ymax = obj['bbox']
-                color = assets.colors[assets.classes.index(label)]
-                frame = cv2.rectangle(
-                    img=frame,
-                    pt1=(xmin, ymin),
-                    pt2=(xmax, ymax),
-                    color=color,
-                    thickness=2
-                )
-                frame = cv2.putText(
-                    img=frame,
-                    text=f'{label} ({str(score)})',
-                    org=(xmin,ymin),
-                    fontFace=cv2.FONT_HERSHEY_SIMPLEX ,
-                    fontScale=0.75,
-                    color=color,
-                    thickness=1,
-                    lineType=cv2.LINE_AA
-                )
+	camera = Camera(
+		device_id=camera_index,
+		flip=0,
+		width=camera_width,
+		height=camera_height,
+		fps=camera_fps
+	)
 
-            cv2.imshow("video frame", frame)
-            if cv2.waitKey(25) & 0xFF == ord('q'):
-                break
-        
-        except KeyboardInterrupt:
-            break
+	command = [
+		'ffmpeg',
+		'-y',
+		'-f', 'rawvideo',
+		'-vcodec', 'rawvideo',
+		'-pix_fmt', 'bgr24',
+		'-s', "{}x{}".format(camera_width, camera_height),
+		'-r', str(camera_fps),
+		'-i', '-',
+		'-c:v', 'libx264',
+		'-pix_fmt', 'yuv420p',
+		'-preset', 'ultrafast', 
+		'-f', 'flv',
+		rtmp_url
+	]
 
-    camera.release()
-    del camera
-    cv2.destroyAllWindows()
+	p = subprocess.Popen(command, stdin=subprocess.PIPE)
+
+	while camera.isReady():
+		frame = camera.read()
+		results = model(frame=frame, classes=classes)
+
+		for result in results:
+			score = result['score']
+			label = result['label']
+			xmin, ymin, xmax, ymax = result['bbox']
+			color = assets.colors[assets.classes.index(label)]
+			frame = cv2.rectangle(
+				img=frame,
+				pt1=(xmin, ymin),
+				pt2=(xmax, ymax),
+				color=color,
+				thickness=2
+			)
+			frame = cv2.putText(
+				img=frame,
+				text=f'{label} ({str(score)})',
+				org=(xmin, ymin),
+				fontFace=cv2.FONT_HERSHEY_SIMPLEX ,
+				fontScale=0.75,
+				color=color,
+				thickness=1,
+				lineType=cv2.LINE_AA
+			)
+
+		p.stdin.write(frame.tobytes())
+
+	camera.release()
+	del camera
 
 
 if __name__ == "__main__":
@@ -68,6 +106,11 @@ if __name__ == "__main__":
     main(
         args.MODEL,
         args.CLASSES,
+        args.STREAM_IP,
+        args.STREAM_PORT,
+        args.STREAM_APPLICATION,
+        args.STREAM_KEY,
+        args.CAMERA_INDEX,
         args.CAMERA_WIDTH,
         args.CAMERA_HEIGHT,
         args.CAMERA_FPS
