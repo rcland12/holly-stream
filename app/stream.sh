74b6ec9bf87e36d2c0bbbea4e8e7e397c91821b2
Russell Land <rcland12@gmail.com>
2024-03-22 02:41:19 -0400

    find a working streaming command for raspberry pi

--- app/stream.sh
+++ app/stream.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
-RTMP_URI=location=rtmp://"$STREAM_IP":"$STREAM_PORT"/"$STREAM_APPLICATION"/"$STREAM_KEY"" live=true"
-gst-launch-1.0 nvarguscamerasrc sensor-id=$CAMERA_INDEX ! \
-'video/x-raw(memory:NVMM)', width=$CAMERA_WIDTH, height=$CAMERA_HEIGHT, framerate=$CAMERA_FPS/1, format=NV12 ! \
-nvv4l2h264enc ! \
-h264parse ! \
-flvmux ! \
-rtmpsink "$RTMP_URI"
+libcamera-vid \
+--framerate $CAMERA_FPS \
+--nopreview \
+--inline \
+-t 0 \
+--width $CAMERA_WIDTH \
+--height $CAMERA_HEIGHT \
+--rotation 180 \
+--listen -o - | \
+ffmpeg \
+-i - \
+-profile:v high \
+-pix_fmt yuvj420p \
+-level:v 4.1 \
+-preset ultrafast \
+-tune zerolatency \
+-vcodec libx264 \
+-r $CAMERA_FPS \
+-s "$CAMERA_WIDTH"x"$CAMERA_HEIGHT" \
+-f flv rtmp://$STREAM_IP:$STREAM_PORT/$STREAM_APPLICATION/$STREAM_KEY
